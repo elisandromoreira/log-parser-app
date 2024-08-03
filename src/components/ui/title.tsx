ce41d8778c95593d6c8d2eb8453d4e7fc15d7e46
Elisandro Moreira <elisandro.moreira@oracle.com>
2024-08-03 15:12:39 -0300

    feat: add interface elements

---
+++ src/components/ui/title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TitleProps {
+  text: string;
+}
+
+const Title: React.FC<TitleProps> = ({ text }) => {
+  return <h1 className="text-3xl font-bold text-center mb-10">{text}</h1>;
+};
+
+export default Title;
